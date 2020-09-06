6a37c19b93429f3b5595595dabc9a5f6ba337e2a
Henrique Inonhe <henriqueinonhe@gmail.com>
2020-09-06 14:22:18 -0300

    Changed fetch URL

--- src/Components/ProfessorsList.tsx
+++ src/Components/ProfessorsList.tsx
@@ -36,7 +36,7 @@ export function ProfessorsList() : JSX.Element
   {
     (async () => 
     {
-      const response = await(fetch(`http://localhost:${process.env.PORT}/professors`));
+      const response = await(fetch(`https://unicamp-scraping.herokuapp.com//professors`));
       const data = await response.json();
       setProfessorsProfiles(data);
       setSelectedProfessorsProfiles(data);
